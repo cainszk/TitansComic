347e9838a00ec3d61dd29616afb64f3ea707a188
CainSZK <shenzhoukai@aliyun.com>
2018-09-09 14:15:15 +0800

    0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace TitansComic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn4001 = new System.Windows.Forms.Button();
+            this.btn4lastpage = new System.Windows.Forms.Button();
+            this.btn4nextpage = new System.Windows.Forms.Button();
+            this.lb4num = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb4history = new System.Windows.Forms.Label();
+            this.btn4002 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn4001
+            // 
+            this.btn4001.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn4001.Location = new System.Drawing.Point(678, 48);
+            this.btn4001.Name = "btn4001";
+            this.btn4001.Size = new System.Drawing.Size(54, 33);
+            this.btn4001.TabIndex = 0;
+            this.btn4001.Text = "001";
+            this.btn4001.Click += new System.EventHandler(this.btn4001_Click);
+            // 
+            // btn4lastpage
+            // 
+            this.btn4lastpage.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn4lastpage.Location = new System.Drawing.Point(678, 812);
+            this.btn4lastpage.Name = "btn4lastpage";
+            this.btn4lastpage.Size = new System.Drawing.Size(242, 106);
+            this.btn4lastpage.TabIndex = 0;
+            this.btn4lastpage.Text = "上一页";
+            this.btn4lastpage.Click += new System.EventHandler(this.btn4lastpage_Click);
+            // 
+            // btn4nextpage
+            // 
+            this.btn4nextpage.Font = new System.Drawing.Font("微软雅黑", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn4nextpage.Location = new System.Drawing.Point(926, 812);
+            this.btn4nextpage.Name = "btn4nextpage";
+            this.btn4nextpage.Size = new System.Drawing.Size(242, 106);
+            this.btn4nextpage.TabIndex = 0;
+            this.btn4nextpage.Text = "下一页";
+            this.btn4nextpage.Click += new System.EventHandler(this.btn4nextpage_Click);
+            // 
+            // lb4num
+            // 
+            this.lb4num.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb4num.Location = new System.Drawing.Point(678, 764);
+            this.lb4num.Name = "lb4num";
+            this.lb4num.Size = new System.Drawing.Size(490, 33);
+            this.lb4num.TabIndex = 3;
+            this.lb4num.Text = "第1话，第1页";
+            this.lb4num.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(678, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(490, 33);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "章节选择";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lb4history
+            // 
+            this.lb4history.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb4history.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lb4history.Location = new System.Drawing.Point(678, 731);
+            this.lb4history.Name = "lb4history";
+            this.lb4history.Size = new System.Drawing.Size(490, 33);
+            this.lb4history.TabIndex = 5;
+            this.lb4history.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btn4002
+            // 
+            this.btn4002.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn4002.Location = new System.Drawing.Point(738, 48);
+            this.btn4002.Name = "btn4002";
+            this.btn4002.Size = new System.Drawing.Size(54, 33);
+            this.btn4002.TabIndex = 0;
+            this.btn4002.Text = "002";
+            this.btn4002.Click += new System.EventHandler(this.btn4002_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(678, 532);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(490, 184);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(644, 906);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1180, 930);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btn4002);
+            this.Controls.Add(this.lb4history);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb4num);
+            this.Controls.Add(this.btn4nextpage);
+            this.Controls.Add(this.btn4lastpage);
+            this.Controls.Add(this.btn4001);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TitansComic v0.1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn4001;
+        private System.Windows.Forms.Button btn4lastpage;
+        private System.Windows.Forms.Button btn4nextpage;
+        private System.Windows.Forms.Label lb4num;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb4history;
+        private System.Windows.Forms.Button btn4002;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TitansComic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string picurl;
+        string epurl;
+        string epname;
+        int picnum;
+        int pgnum;
+        string lstx;
+        string x;
+        string y;
+        void lastpage()
+        {
+            if (pgnum > 1)
+            {
+                picnum -= 1;
+                see();
+            }
+        }
+        void nextpage()
+        {
+            picnum += 1;
+            see();
+        }
+        void see()
+        {
+            picurl = epurl + picnum.ToString();
+            string url = string.Format(picurl + ".jpg");
+            System.Net.WebRequest webreq = System.Net.WebRequest.Create(url);
+            System.Net.WebResponse webres = webreq.GetResponse();
+            using (System.IO.Stream stream = webres.GetResponseStream())
+            {
+                pictureBox1.Image = Image.FromStream(stream);
+            }
+            switch (epurl)
+            {
+                case "http://p17.xiaoshidi.net/2013/03/":
+                    switch (epname)
+                    {
+                        case "ep001":
+                            pgnum = picnum - 171233319;
+                            lb4num.Text = "第1话，第" + pgnum.ToString() + "页";
+                            break;
+                        case "ep002":
+                            pgnum = picnum - 171234349;
+                            lb4num.Text = "第2话，第" + pgnum.ToString() + "页";
+                            break;
+                    }
+                    break;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(@"book.mark"))
+            {
+                FileStream fsrd = File.OpenRead(@"book.mark");
+                StreamReader sr = new StreamReader(fsrd, System.Text.Encoding.Default);
+                string strrd = sr.ReadToEnd();
+                sr.Close();
+                fsrd.Close();
+                char[] separator = { ':' };
+                string[] splitstrings = new string[100];
+                splitstrings = strrd.Split(separator);
+                for (int i = 0; i < splitstrings.Length; i++)
+                {
+                    x = splitstrings[0];
+                    y = splitstrings[1];
+                }
+                epname = x;
+                switch (x)
+                {
+                    case "ep001":
+                        lstx = "第1话";
+                        epurl = "http://p17.xiaoshidi.net/2013/03/";
+                        picnum = Int32.Parse(y) + 171233319;
+                        break;
+                    case "ep002":
+                        lstx = "第2话";
+                        epurl = "http://p17.xiaoshidi.net/2013/03/";
+                        picnum = Int32.Parse(y) + 171234349;
+                        break;
+                }
+                lb4history.Text = "上次看到：" + lstx + "，第" + y + "页";
+                see();
+
+            }
+        }
+        private void btn4001_Click(object sender, EventArgs e)
+        {
+            epname = "ep001";
+            picnum = 171233320;
+            pgnum = picnum - 171233319;
+            epurl = "http://p17.xiaoshidi.net/2013/03/";
+            see();
+        }
+        private void btn4lastpage_Click(object sender, EventArgs e)
+        {
+            lastpage();
+        }
+        private void btn4nextpage_Click(object sender, EventArgs e)
+        {
+            nextpage();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FileStream fscr = File.Create(@"book.mark");
+            StreamWriter sw = new StreamWriter(fscr, System.Text.Encoding.Default);
+            sw.Write(epname+":"+ pgnum);
+            sw.Close();
+            fscr.Close();
+        }
+        private void btn4002_Click(object sender, EventArgs e)
+        {
+            epname = "ep002";
+            picnum = 171234350;
+            pgnum = picnum - 171234349;
+            epurl = "http://p17.xiaoshidi.net/2013/03/";
+            see();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                lastpage();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                nextpage();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                lastpage();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                nextpage();
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                nextpage();
+            }
+        }
+    }
+}
